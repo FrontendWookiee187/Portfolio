0cb6485ff0ecd9a3040ca58c2a61c37e01cdcd30
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-05-26 17:40:53 +0200

    everything deleted and completely new structure

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MainPageComponent } from './main-page/main-page.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainPageComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

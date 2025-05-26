d33ed9a29ae8bf08c3d55aeb89a29e3991ed27af
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-05-26 17:09:46 +0200

    try to otpimize the code

--- portfolio/src/app/main-page/main-page.component.ts
+++ portfolio/src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })

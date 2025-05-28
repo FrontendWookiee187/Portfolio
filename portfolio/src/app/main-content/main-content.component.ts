fd93dbeec2775c2dc32e221b3ff397dc0a59ba58
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-05-28 16:58:00 +0200

    added my skills component

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { WhyMeComponent } from "./why-me/why-me.component";
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, WhyMeComponent],
+  imports: [CommonModule, LandingPageComponent, WhyMeComponent, MySkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

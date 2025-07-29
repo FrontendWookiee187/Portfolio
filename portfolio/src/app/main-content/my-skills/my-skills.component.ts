b721a99bec1ffbb9ab2d3937721ff615ed4abb87
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-07-29 11:18:18 +0200

    Add new skill images and update skills list
    Introduced a Skill interface and updated the MySkillsComponent to use an array of skills with associated images. Added new images for HTML, CSS, JavaScript, Angular, TypeScript, Git, Firebase, REST API, and Scrum to the assets directory.

--- portfolio/src/app/main-content/my-skills/my-skills.component.ts
+++ portfolio/src/app/main-content/my-skills/my-skills.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 
+interface Skill {
+  name: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-my-skills',
   standalone: true,
@@ -9,5 +14,16 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
-
+  /** Array of main technical skills */
+  skills: Skill[] = [
+    { name: 'HTML', image: 'assets/img/myskills/html_new.png' },
+    { name: 'CSS', image: 'assets/img/myskills/css_new.png' },
+    { name: 'JavaScript', image: 'assets/img/myskills/javascript_new.png' },
+    { name: 'Angular', image: 'assets/img/myskills/angular_new.png' },
+    { name: 'TypeScript', image: 'assets/img/myskills/typescript_new.png' },
+    { name: 'Git', image: 'assets/img/myskills/git_new.png' },
+    { name: 'Firebase', image: 'assets/img/myskills/firebase_new.png' },
+    { name: 'REST API', image: 'assets/img/myskills/api_new.png' },
+    { name: 'Scrum', image: 'assets/img/myskills/scrum.png' }
+  ];
 }

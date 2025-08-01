d4d9aa37ef99aff7f685d5cacceae766973aaffd
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-08-01 13:03:41 +0200

    Show landing page conditionally based on route
    Added logic to display the landing page only when the current route does not include '/imprint'. Updated the template to use *ngIf for the landing page component and adjusted the component to listen to router events. Also made the navbar sticky for improved UI.

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,31 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { MainContentComponent } from './main-content/main-content.component';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { WhyMeComponent } from './main-content/why-me/why-me.component';
 import {
-    TranslateService,
-    TranslatePipe,
-    TranslateDirective
+    TranslateService
 } from "@ngx-translate/core";
 import { NavbarComponent } from './main-content/landing-page/navbar/navbar.component';
 import { LandingPageComponent } from './main-content/landing-page/landing-page.component';
+import { filter } from 'rxjs/operators';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent, WhyMeComponent, TranslatePipe, TranslateDirective, NavbarComponent, LandingPageComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, NavbarComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
-  constructor(private translate: TranslateService) {
+  showLandingPage = true;
+
+  constructor(private translate: TranslateService, private router: Router) {
         this.translate.addLangs(['de', 'en']);
         this.translate.setDefaultLang('en');
         this.translate.use('en');
+
+        // Listen to router events to determine if landing page should be shown
+        this.router.events.pipe(
+          filter(event => event instanceof NavigationEnd)
+        ).subscribe((event) => {
+          if (event instanceof NavigationEnd) {
+            this.showLandingPage = !event.url.includes('/imprint');
+          }
+        });
     }
 }
